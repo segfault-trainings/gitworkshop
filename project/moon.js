1282207cf78ee483f3c4c029af393971767dc73c
André Roth <neolynx@gmail.com>
2015-11-01 12:07:43 +0100

    project: renice code

--- project/moon.js
+++ project/moon.js
@@ -1,132 +1,93 @@
-var container;
-
-var camera, scene, renderer, objects;
-var particleLight;
+var camera, scene, texture, particleLight, renderer;
+var moons = [];
 
 init();
+moon( 0x00ff00 );
+moon( 0x00ffff );
 animate();
+
+function moon( color )
+{
+  m = new THREE.MeshPhongMaterial( { map: texture,
+    bumpMap: texture, bumpScale: 5, color: color,
+    specular: 0x333333, shininess: 10, metal: true, shading: THREE.SmoothShading } );
+  sphere = new THREE.Mesh( new THREE.SphereBufferGeometry( 70, 32, 16 ), m );
+  pos = moons.length +1
+  sphere.position.x = ( pos % 4 ) * 200 - 200;
+  sphere.position.z = Math.floor( pos / 4 ) * 200 - 200;
+  moons.push( sphere );
+  scene.add( sphere );
+}
 
 function init( )
 {
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
+  // Container
+  container = document.createElement( 'div' );
+  document.body.appendChild( container );
 
-    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 2000 );
-    camera.position.set( 0, 200, 0 );
+  // Camera
+  camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 2000 );
+  camera.position.set( 0, 200, 0 );
+  scene = new THREE.Scene();
 
-    scene = new THREE.Scene();
+  // Texture
+  texture = THREE.ImageUtils.loadTexture( "moon_1024.jpg" );
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  texture.anisotropy = 16;
 
-    // Materials
+  // Lights
+  particleLight = new THREE.Mesh( new THREE.SphereBufferGeometry( 4, 8, 8 ),
+                                  new THREE.MeshBasicMaterial( { color: 0xffff00 } ));
+  scene.add( particleLight );
+  scene.add( new THREE.AmbientLight( 0xa0a0a0, 5 ));
+  var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  directionalLight.position.set( 1, 1, 1 ).normalize();
+  scene.add( directionalLight );
+  var pointLight = new THREE.PointLight( 0xffff00, 2, 800 );
+  particleLight.add( pointLight );
 
-    var shading = THREE.SmoothShading;
+  // Render
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setClearColor( 0x000000 );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.sortObjects = true;
+  container.appendChild( renderer.domElement );
+  renderer.gammaInput = true;
+  renderer.gammaOutput = true;
 
-    var materials = [];
-
-
-    var imgTexture2 = THREE.ImageUtils.loadTexture( "moon_1024.jpg" );
-    imgTexture2.wrapS = imgTexture2.wrapT = THREE.RepeatWrapping;
-    imgTexture2.anisotropy = 16;
-
-    materials.push( new THREE.MeshPhongMaterial( { map: imgTexture2, bumpMap: imgTexture2, bumpScale: 1, color: 0x00ff00, specular: 0x333333, shininess: 10, metal: true, shading: shading } ));
-
-    // Spheres geometry
-
-    var geometry_smooth = new THREE.SphereBufferGeometry( 70, 32, 16 );
-
-    objects = [];
-
-    var sphere, geometry, material;
-
-    for ( var i = 0, l = materials.length; i < l; i ++ )
-    {
-
-        material = materials[ i ];
-
-        sphere = new THREE.Mesh( geometry_smooth, material );
-
-        sphere.position.x = ( i % 4 ) * 200 - 200;
-        sphere.position.z = Math.floor( i / 4 ) * 200 - 200;
-
-        objects.push( sphere );
-
-        scene.add( sphere );
-
-    }
-
-    particleLight = new THREE.Mesh( new THREE.SphereBufferGeometry( 4, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0xffffff } ) );
-    scene.add( particleLight );
-
-    // Lights
-
-    scene.add( new THREE.AmbientLight( 0xa0a0a0, 5 ));
-
-    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-    directionalLight.position.set( 1, 1, 1 ).normalize();
-    scene.add( directionalLight );
-
-    var pointLight = new THREE.PointLight( 0xffffff, 2, 800 );
-    particleLight.add( pointLight );
-
-    //
-
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setClearColor( 0x000000 );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.sortObjects = true;
-
-    container.appendChild( renderer.domElement );
-
-    renderer.gammaInput = true;
-    renderer.gammaOutput = true;
-
-    //
-
-    window.addEventListener( 'resize', onWindowResize, false );
-
+  // Resize
+  window.addEventListener( 'resize', onWindowResize, false );
 }
 
-function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
+function onWindowResize()
+{
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-//
-
-function animate() {
-
-    requestAnimationFrame( animate );
-
-    render();
-
+function animate()
+{
+  requestAnimationFrame( animate );
+  render();
 }
 
-function render() {
+function render()
+{
+  var timer = Date.now() * 0.00025;
+  camera.position.x = Math.cos( timer ) * 800;
+  camera.position.z = Math.sin( timer ) * 800;
+  camera.lookAt( scene.position );
 
-    var timer = Date.now() * 0.00025;
+  // make the moons spin
+  for( var i = 0, l = moons.length; i < l; i ++ )
+    moons[i].rotation.y += 0.005;
 
-    camera.position.x = Math.cos( timer ) * 800;
-    camera.position.z = Math.sin( timer ) * 800;
+  particleLight.position.x = Math.sin( timer * 7 ) * 300;
+  particleLight.position.y = Math.cos( timer * 5 ) * 400;
+  particleLight.position.z = Math.cos( timer * 3 ) * 300;
 
-    camera.lookAt( scene.position );
-
-    for ( var i = 0, l = objects.length; i < l; i ++ ) {
-
-        var object = objects[ i ];
-
-        object.rotation.y += 0.005;
-
-    }
-
-    particleLight.position.x = Math.sin( timer * 7 ) * 300;
-    particleLight.position.y = Math.cos( timer * 5 ) * 400;
-    particleLight.position.z = Math.cos( timer * 3 ) * 300;
-
-    renderer.render( scene, camera );
-
+  renderer.render( scene, camera );
 }
 
